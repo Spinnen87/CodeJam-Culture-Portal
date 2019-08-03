d0b83c1f0f0e182307736a68fe3b8dbc62db7fed
Alexandrina <lizurchik.a@gmail.com>
2019-08-03 15:38:58 +0300

    feat: add page "About"

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Developers from '../components/developers';
+
+const about = () => {
+  return (
+
+    <Layout>
+      <Developers developers={'Developers'} />
+    </Layout>
+
+  );
+};
+
+export default about;
