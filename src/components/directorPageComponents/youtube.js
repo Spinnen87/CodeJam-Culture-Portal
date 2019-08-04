cbc5382def8664453394453f421d5530c67f7d34
aksr666 <aksr100793@mail.ru>
2019-08-04 03:59:24 +0300

    feat: add popup for youtube

--- src/components/directorPageComponents/youtube.js
+++ src/components/directorPageComponents/youtube.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import YouTube from 'react-youtube';
+import 'react-modal-video/css/modal-video.min.css';
+import ModalVideo from 'react-modal-video';
+import Typography from '@material-ui/core/Typography';
+import { translate } from 'react-i18next';
 
-const Youtube = ({ data }) => <YouTube videoId={data} opts={{ playerVars: { autoplay: 0 } }} />;
+class Youtube extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
 
-export default Youtube;
+    render() {
+        return (
+            <div>
+                <Typography variant="h6" color="textSecondary" component="p" >
+                    {'Youtube'}
+                </Typography>
+                <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId={this.props.data} onClose={() => this.setState({ isOpen: false })} />
+                <button onClick={() => this.setState({ isOpen: true })}>{this.props.t('Show video')}</button>
+            </div>
+        )
+    }
+}
+
+export default translate()(Youtube); 
